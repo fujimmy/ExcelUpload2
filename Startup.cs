736e46971467a80deddb95fd8a30823a65645461
JimmyFu <emprorgame@gmail.com>
2018-02-09 18:00:47 +0800

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExcelUpload.Startup))]
+namespace ExcelUpload
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
